5974a09a727e65e1130d300c5c3aaf36977a468c
destiny1017 <destiny1017@nate.com>
2021-05-01 23:56:03 +0900

    평균은 넘겠지

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test05.java
@@ -0,0 +1,50 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test05 {
+	
+//	# 평균은 넘겠지
+//	
+//	문제
+//	대학생 새내기들의 90%는 자신이 반에서 평균은 넘는다고 생각한다. 당신은 그들에게 슬픈 진실을 알려줘야 한다.
+//
+//	입력
+//	첫째 줄에는 테스트 케이스의 개수 C가 주어진다.
+//
+//	둘째 줄부터 각 테스트 케이스마다 학생의 수 N(1 ≤ N ≤ 1000, N은 정수)이 첫 수로 주어지고, 이어서 N명의 점수가 주어진다. 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+//
+//	출력
+//	각 케이스마다 한 줄씩 평균을 넘는 학생들의 비율을 반올림하여 소수점 셋째 자리까지 출력한다.
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int length = sc.nextInt();
+		float[] answers = new float[length]; 
+		for (int j = 0; j < length; j++) {
+			int[] scores = new int[sc.nextInt()];
+			int sum = 0;
+			float average;
+			int upAver = 0;
+			float upAverRatio = 0; 
+			for (int i = 0; i < scores.length; i++) {
+				scores[i] = sc.nextInt();
+				sum += scores[i];
+			}
+			average = (float)sum/scores.length;
+			for (int i = 0; i < scores.length; i++) {
+				if(scores[i] > average) upAver++;
+			}
+			upAverRatio = ((float)upAver/scores.length)*100;
+//			System.out.println(Arrays.toString(scores));
+//			System.out.println("합계 : " + sum + ", 평균 : " + average);
+//			System.out.println("평균 이상 학생 수 : " + upAver + ", 평균 이상 학생 비율 : " + upAverRatio);
+			answers[j] = upAverRatio;
+		}
+		for (int i = 0; i < answers.length; i++) {
+			System.out.printf("%.3f%%", answers[i]);
+			System.out.println();
+		}
+	}
+
+}
