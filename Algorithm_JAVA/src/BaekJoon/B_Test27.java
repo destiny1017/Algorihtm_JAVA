a20902857ad10d93bb60c3b5481d0b886c0a6112
destiny1017 <destiny1017@nate.com>
2021-05-31 23:59:04 +0900

    막대기

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test27.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test27 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		int cnt = 1;
+		int len = 64;
+		while(true) {
+			if(num >= 64) {
+				
+			}
+		}
+
+	}
+
+}
