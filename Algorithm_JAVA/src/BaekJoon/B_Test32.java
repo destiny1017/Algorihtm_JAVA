e17007477fb360c41a142620ada8a1a424faf8b4
destiny1017 <destiny1017@nate.com>
2021-06-06 23:59:20 +0900

    링

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test32.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test32 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int first;
+		for (int i = 0; i < sc.nextInt(); i++) {
+			int spin = 0;
+			if(i==0) first = sc.nextInt();
+			else {
+				
+			}
+		}
+
+	}
+
+}
