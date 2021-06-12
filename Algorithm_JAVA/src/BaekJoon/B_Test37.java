9288247da62cb08c3310342957b103121e4d9ac2
destiny1017 <destiny1017@nate.com>
2021-06-12 23:59:35 +0900

    TGN

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test37.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test37 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for (int i = 0; i < num; i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt() - sc.nextInt();
+			if(a == b) {
+				
+			}
+		}
+
+	}
+
+}
