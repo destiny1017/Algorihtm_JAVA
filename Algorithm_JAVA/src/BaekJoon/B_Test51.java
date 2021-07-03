8beecebe08064cb56c393ec8b1e8e8cd837b2da5
destiny1017 <destiny1017@nate.com>
2021-07-03 23:59:36 +0900

    한 수

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test51.java
@@ -0,0 +1,21 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test51 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt(), cnt = 0;
+		for (int i = 0; i < num; i++) {
+			String strNum = num+"";
+			for (int j = 0; j < strNum.length; j++) {
+				if(strNum.substring(j, j+1) == 0) {
+					
+				}
+			}
+		}
+
+	}
+
+}
