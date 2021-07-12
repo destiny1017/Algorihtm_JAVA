3222a94964802fa8d54602f31f8bad41dbf425c4
destiny1017 <destiny1017@nate.com>
2021-07-12 23:59:27 +0900

    1,2,3 더하기

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test59.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test59 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		int arr[] = {1,2,3};
+		for (int i = 0; i < len; i++) {
+			int num = sc.nextInt();
+			for (int j = 0; j < arr.length; j++) {
+				int sum = 0;
+				
+			}
+		}
+	}
+
+}
