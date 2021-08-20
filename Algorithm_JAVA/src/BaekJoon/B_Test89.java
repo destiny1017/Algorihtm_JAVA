4114d29db34ae9ce92f354fda716271ea6a99583
destiny1017 <destiny1017@nate.com>
2021-08-20 23:59:21 +0900

    16진수풀이중

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test89.java
@@ -0,0 +1,23 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test89 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char hex[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+//		char num[] = sc.next().toCharArray();
+		String num = sc.next();
+		int cnt = 0;
+		String rs = "";
+		while(true) {
+			
+			if(num.equals(rs)) break;
+		}
+//		char a[] = {'1','2'};
+//		char b[] = {'1','2'};
+//		System.out.println(a.equals(b));
+	}
+
+}
