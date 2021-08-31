f129d80ff9f8794957711d4ef396fec8c9755ea5
destiny1017 <destiny1017@nate.com>
2021-08-31 23:59:51 +0900

    백설공주와 일곱난쟁이 진행중

--- Algorithm_JAVA/src/BaekJoon/B_Test98.java
+++ Algorithm_JAVA/src/BaekJoon/B_Test98.java
@@ -1,10 +1,20 @@
 package BaekJoon;
 
+import java.util.Scanner;
+
 public class B_Test98 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		int hats[] = new int[9];
+		for (int i = 0; i < hats.length; i++) {
+			hats[i] = sc.nextInt();
+		}
+		for (int i = 0; i < hats.length; i++) {
+			for (int j = 0; j < hats.length; j++) {
+				
+			}
+		}
 	}
 
 }
