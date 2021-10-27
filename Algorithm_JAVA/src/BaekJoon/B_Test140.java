7b0c90530eda3c562376627e773854ca7c88548e
KDH_Notebook <destiny1017@nate.com>
2021-10-27 23:59:35 +0900

    공

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test140.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test140 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		for (int i = 0; i < len; i++) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+		}
+	}
+
+}
