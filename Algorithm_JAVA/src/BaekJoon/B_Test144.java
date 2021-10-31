394db21f24fdc944da095c5325ad3be68bc6ec00
destiny1017 <destiny1017@nate.com>
2021-10-31 23:59:07 +0900

    트럭 주차

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test144.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test144 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int price = 0;
+		int st[] = new int[3];
+		int ed[] = new int[3];
+		for (int i = 0; i < 3; i++) {
+			
+		}
+	}
+
+}
