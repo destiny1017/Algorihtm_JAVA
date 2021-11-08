391b17419d9ded98f673aa87a9c012bf05d39d9f
destiny1017 <destiny1017@nate.com>
2021-11-08 23:59:34 +0900

    벼락치기

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test152.java
@@ -0,0 +1,23 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test152 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		int time = sc.nextInt();
+		int stTime[] = new int[len];
+		int point[] = new int[len];
+		for (int i = 0; i < len; i++) {
+			stTime[i] = sc.nextInt();
+			point[i] = sc.nextInt();
+		}
+		for (int i = 0; i < len; i++) {
+			
+		}
+
+	}
+
+}
