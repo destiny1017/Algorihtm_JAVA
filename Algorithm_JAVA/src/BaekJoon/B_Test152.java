b6d24954ae13ca8ba86385abd57d19f6da508fe8
destiny1017 <destiny1017@nate.com>
2021-11-09 23:56:20 +0900

    벼락치기 진행중

--- Algorithm_JAVA/src/BaekJoon/B_Test152.java
+++ Algorithm_JAVA/src/BaekJoon/B_Test152.java
@@ -1,5 +1,6 @@
 package BaekJoon;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class B_Test152 {
@@ -10,13 +11,43 @@ public class B_Test152 {
 		int time = sc.nextInt();
 		int stTime[] = new int[len];
 		int point[] = new int[len];
+		double eff[] = new double[len];
+//		int effRank[] = new int[len];
 		for (int i = 0; i < len; i++) {
 			stTime[i] = sc.nextInt();
 			point[i] = sc.nextInt();
+			eff[i] = (double) point[i] / stTime[i];
 		}
 		for (int i = 0; i < len; i++) {
-			
+			for (int j = 1; j < len; j++) {
+				if(eff[j] > eff[i]) {
+					double tmpEff = eff[i];
+					int tmpStTime = stTime[i];
+					int tmpPoint = point[i];
+					
+					eff[i] = eff[j];
+					stTime[i] = stTime[j];
+					point[i] = point[j];
+					
+					eff[j] = tmpEff;
+					stTime[j] = tmpStTime;
+					point[j] = tmpPoint;
+				}
+			}
 		}
+		int useHour = 0;
+		int totalPoint = 0;
+		for (int i = 0; i < eff.length; i++) {
+			if(useHour + stTime[i] > time) {
+				continue;
+			}
+			useHour += stTime[i];
+			totalPoint += point[i];
+		}
+		System.out.println(Arrays.toString(eff));
+		System.out.println(Arrays.toString(stTime));
+		System.out.println(Arrays.toString(point));
+		System.out.println(totalPoint);
 
 	}
 
