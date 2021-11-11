6902ceaaf5db68c3aa8c44bf29ac38da030474f6
destiny1017 <destiny1017@nate.com>
2021-11-11 23:59:51 +0900

    이진수 연산

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test153.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test153 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char a[] = sc.next().toCharArray();
+		char b[] = sc.next().toCharArray();
+	}
+
+}
