e2d52433b70d439d1e583a3fedec4b8f66fdead0
destiny1017 <destiny1017@nate.com>
2021-11-23 23:59:33 +0900

    남욱이의 닭장

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test160.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test160 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		for (int i = 0; i < len; i++) {
+			int m = sc.nextInt();
+			int n = sc.nextInt();
+		}
+	}
+
+}
