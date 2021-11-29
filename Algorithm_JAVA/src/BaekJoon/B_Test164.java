b503b55896d6b19f0f337120369b79d12e0c2642
destiny1017 <destiny1017@nate.com>
2021-11-29 23:59:50 +0900

    우유 축제

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test164.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test164 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		int next = 0;
+		int cnt = 0;
+		for (int i = 0; i < len; i++) {
+			int store = sc.nextInt();
+			if(next == store) {
+				
+			}
+		}
+	}
+
+}
