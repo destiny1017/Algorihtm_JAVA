6294d7f71e481ab32ef10ba0279d3bc4374fd034
destiny1017 <destiny1017@nate.com>
2021-11-30 23:59:42 +0900

    닉네임에 갓 붙이기

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test165.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test165 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		for (int i = 0; i < len; i++) {
+			String name[] = sc.nextLine().split(" ");
+			
+		}
+	}
+
+}
