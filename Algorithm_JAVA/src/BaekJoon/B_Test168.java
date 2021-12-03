a7e54e8d2a0a1b9af6d2412eac9e9b691f5361fb
destiny1017 <destiny1017@nate.com>
2021-12-03 23:59:27 +0900

    Doubles

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test168.java
@@ -0,0 +1,14 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test168 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			list = new ArrayList<Integer>();
+		}
+	}
+
+}
