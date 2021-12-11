426845c889cab234273f8669bb40596e8e5634f0
destiny1017 <destiny1017@nate.com>
2021-12-11 23:48:26 +0900

    피카츄

--- Algorithm_JAVA/src/BaekJoon/B_Test174.java
+++ Algorithm_JAVA/src/BaekJoon/B_Test174.java
@@ -7,7 +7,25 @@ public class B_Test174 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String word = sc.next();
-		
+		while(true) {
+			if(word.indexOf("pi") > -1) {
+				word = word.replaceFirst("pi", "xx");
+				continue;
+			} else if(word.indexOf("ka") > -1) {
+				word = word.replaceFirst("ka", "xx");
+				continue;
+			} else if(word.indexOf("chu") > -1) {
+				word = word.replaceFirst("chu", "xxx");
+				continue;
+			}
+			int rst = 1;
+			char chars[] = word.toCharArray();
+			for (int i = 0; i < chars.length; i++) {
+				if(chars[i] != 'x') rst = 0; 
+			}
+			System.out.println(rst == 0 ? "NO" : "YES");
+			break;
+		}
 	}
 
 }
