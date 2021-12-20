62cd0e45e11f531e9a7c300298c8289ea8e917a5
destiny1017 <destiny1017@nate.com>
2021-12-21 00:19:03 +0900

    별 찍기 - 20

--- Algorithm_JAVA/src/BaekJoon/B_Test181.java
+++ Algorithm_JAVA/src/BaekJoon/B_Test181.java
@@ -3,17 +3,36 @@ package BaekJoon;
 import java.util.Scanner;
 
 public class B_Test181 {
+	
+//	# 별 찍기 - 20
+//	
+//	문제
+//	예제를 보고 규칙을 유추한 뒤에 별을 찍어 보세요.
+//	
+//	N = 3
+//	
+//	* * *
+//	 * * *
+//	* * *
+//
+//	입력
+//	첫째 줄에 N(1 ≤ N ≤ 100)이 주어진다.
+//
+//	출력
+//	첫째 줄부터 차례대로 별을 출력한다.
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		for (int i = 0; i < n; i++) {
+			String line = "";
+			int j = 0;
 			if(i%2 == 0) {
-				
-			} else {
-				String line = "*";
-				for (int j = 1; j < n; j++) line += " *"; 
+				line = "*";
+				j++;
 			}
+			for (j += 0; j < n; j++) line += " *"; 
+			System.out.println(line);
 		}
 	}
 
