e55f50676431a9d1a4bae42764610e784799c143
destiny1017 <destiny1017@nate.com>
2021-12-28 23:59:19 +0900

    가위 바위 보?

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test186.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test186 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		for (int i = 0; i < 3; i++) {
+			int num = sc.nextInt();
+			for (int j = 0; j < num; j++) {
+				char p1 = sc.next().charAt(0);
+				char p2 = sc.next().charAt(0);
+			}
+		}
+	}
+
+}
