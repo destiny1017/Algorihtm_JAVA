d3993a590baa8b4762bbb2e92bf60805c896c43a
destiny1017 <destiny1017@nate.com>
2022-01-10 23:59:24 +0900

    아이들은 사탕을 좋아해

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test195.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test195 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		for (int i = 0; i < len; i++) {
+			
+		}
+	}
+
+}
