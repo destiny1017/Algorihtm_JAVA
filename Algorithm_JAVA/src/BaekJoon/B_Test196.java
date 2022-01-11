00e2b50fb35177ba5f603f16b20e07edd616cdc5
destiny1017 <destiny1017@nate.com>
2022-01-11 23:59:35 +0900

    창영이의 일기장

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test196.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test196 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char chars[] = sc.next().toCharArray();
+	}
+
+}
