8e3895ba9afa48c69a287f8f8eff2354a177f582
destiny1017 <destiny1017@nate.com>
2022-01-25 23:59:57 +0900

    숫자맞주기 게임

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test207.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test207 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while(true) {
+			int n = sc.nextInt();
+			if(n == 0) break;
+			int n1 = 3*n;
+			if(n1%2 == 0) {
+				
+			}
+		}
+	}
+
+}
