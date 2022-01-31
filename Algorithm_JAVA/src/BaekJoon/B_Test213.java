6ef57fa051659c56fcac16af160adef36ab7b4bb
destiny1017 <destiny1017@nate.com>
2022-01-31 23:59:23 +0900

    인접한 비트의 개수

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test213.java
@@ -0,0 +1,17 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test213 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextInt();
+		for (int i = 0; i < len; i++) {
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			
+		}
+	}
+
+}
