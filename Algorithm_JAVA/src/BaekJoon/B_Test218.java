aa699be21bd13e68ad128fd0ae7a8e1c6f286a81
destiny1017 <destiny1017@nate.com>
2022-02-11 23:58:41 +0900

    백준 1level 문제들

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test218.java
@@ -0,0 +1,58 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B_Test218 {
+	
+	
+	
+//	// 시험 성적
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int result = sc.nextInt();
+//		char rank;
+//		if(result > 89) rank = 'A';
+//		else if(result > 79) rank = 'B';
+//		else if(result > 69) rank = 'C';
+//		else if(result > 59) rank = 'D';
+//		else rank = 'F';
+//		System.out.println(rank);
+//	}
+	
+//	// 두 수 비교하기
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		String result = "";
+//		if(a > b) result = ">";
+//		else if(a < b) result = "<";
+//		else result = "==";
+//		System.out.println(result);
+//	}
+	
+//	// A-B
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println(sc.nextInt() - sc.nextInt());
+//	}
+	
+//	// 사칙연산
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		System.out.println(a+b);
+//		System.out.println(a-b);
+//		System.out.println(a*b);
+//		System.out.println(a/b);
+//		System.out.println(a%b);
+//	}
+
+//	// A/B
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println(sc.nextDouble()/sc.nextDouble());
+//	}
+
+}
