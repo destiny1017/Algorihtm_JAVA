8a1faba306a6dd9b5abaa9dc877db5b238d64aa9
destiny1017 <destiny1017@nate.com>
2022-02-13 23:59:34 +0900

    백준 브5문제

--- Algorithm_JAVA/src/BaekJoon/B_Test218.java
+++ Algorithm_JAVA/src/BaekJoon/B_Test218.java
@@ -4,7 +4,45 @@ import java.util.Scanner;
 
 public class B_Test218 {
 	
+//	// 나부 함대 데이터
+//	public static void main(String[] args) {
+//		String str = "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n" + 
+//				"N2 Bomber      Heavy Fighter  Limited    21        \n" + 
+//				"J-Type 327     Light Combat   Unlimited  1         \n" + 
+//				"NX Cruiser     Medium Fighter Limited    18        \n" + 
+//				"N1 Starfighter Medium Fighter Unlimited  25        \n" + 
+//				"Royal Cruiser  Light Combat   Limited    4         ";
+//		System.out.println(str);
+//	}
 	
+//	// 스타워즈
+//	public static void main(String[] args) {
+//		String str = "    8888888888  888    88888\n" + 
+//				"   88     88   88 88   88  88\n" + 
+//				"    8888  88  88   88  88888\n" + 
+//				"       88 88 888888888 88   88\n" + 
+//				"88888888  88 88     88 88    888888\n" + 
+//				"\n" + 
+//				"88  88  88   888    88888    888888\n" + 
+//				"88  88  88  88 88   88  88  88\n" + 
+//				"88 8888 88 88   88  88888    8888\n" + 
+//				" 888  888 888888888 88  88      88\n" + 
+//				"  88  88  88     88 88   88888888";
+//		System.out.println(str);
+//	}
+	
+//	// ??!
+//	public static void main(String[] args) {
+//		System.out.println(new Scanner(System.in).next() + "??!");
+//	}
+	
+//	// A + B - 4
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		while(sc.hasNext()) {
+//			System.out.println(sc.nextInt() + sc.nextInt());
+//		}
+//	}
 	
 //	// 시험 성적
 //	public static void main(String[] args) {
