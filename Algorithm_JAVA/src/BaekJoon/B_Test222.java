ce463001c1f83c76810571490febd9ec4e486c89
destiny1017 <destiny1017@nate.com>
2022-02-17 00:48:03 +0900

    약수

--- Algorithm_JAVA/src/BaekJoon/B_Test222.java
+++ Algorithm_JAVA/src/BaekJoon/B_Test222.java
@@ -4,6 +4,19 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class B_Test222 {
+	
+//	# 약수
+//	
+//	문제
+//	양수 A가 N의 진짜 약수가 되려면, N이 A의 배수이고, A가 1과 N이 아니어야 한다. 
+//	어떤 수 N의 진짜 약수가 모두 주어질 때, N을 구하는 프로그램을 작성하시오.
+//
+//	입력
+//	첫째 줄에 N의 진짜 약수의 개수가 주어진다. 이 개수는 50보다 작거나 같은 자연수이다. 
+//	둘째 줄에는 N의 진짜 약수가 주어진다. 1,000,000보다 작거나 같고, 2보다 크거나 같은 자연수이고, 중복되지 않는다.
+//
+//	출력
+//	첫째 줄에 N을 출력한다. N은 항상 32비트 부호있는 정수로 표현할 수 있다.
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -11,13 +24,7 @@ public class B_Test222 {
 		int arr[] = new int[len];
 		for (int i = 0; i < len; i++) arr[i] = sc.nextInt();
 		Arrays.sort(arr);
-		int num = arr[arr.length-1];
-		while(true) {
-			boolean flag = false;
-			for (int i = 0; i < arr.length; i++) {
-				
-			}
-		}
+		System.out.println(arr[0] * arr[arr.length-1]);
 	}
 
 }
