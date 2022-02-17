ee3168b74c490018ac887128a957df6a475bdbad
KDH_Notebook <destiny1017@nate.com>
2022-02-17 20:18:44 +0900

    무의미한 브5문제풀이

--- Algorithm_JAVA/src/BaekJoon/B_Test219.java
+++ Algorithm_JAVA/src/BaekJoon/B_Test219.java
@@ -1,9 +1,88 @@
 package BaekJoon;
 
 import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class B_Test219 {
+	
+	// 큰 수 곱셈
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		BigInteger bi1 = new BigInteger(sc.next());
+//		BigInteger bi2 = new BigInteger(sc.next());
+//		System.out.print(bi1.multiply(bi2));
+//	}
+	
+	// 고려대는 사랑입니다
+//	public static void main(String[] args) {
+//		System.out.println("고려대학교");
+//	}
+	
+	// 아스키 코드
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		char a = sc.next().charAt(0);
+//		System.out.println((int)a);
+//	}
+	
+	// 오늘 날짜
+//	public static void main(String[] args) {
+//		Date date = new Date();
+//		SimpleDateFormat df = new SimpleDateFormat("YYYY-MM-dd");
+//		System.out.println(df.format(date));
+//	}
+	
+//	// Next in line
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int a = sc.nextInt();
+//		int b = sc.nextInt();
+//		int c = sc.nextInt();
+//		System.out.printf("%d\n%d\n%d\n%d", (a+b)%c, ((a%c)+(b%c))%c, (a*b)%c, ((a%c)*(b%c))%c);
+//	}
+	
+	// A + B
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println(sc.nextInt() + sc.nextInt());
+//	}
+	
+	// 엄청난 부자2
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		BigInteger bi1 = new BigInteger(sc.next());
+//		BigInteger bi2 = new BigInteger(sc.next());
+//		System.out.printf("%s\n%s", bi1.divide(bi2), bi1.remainder(bi2));
+//	}
+	
+	// 긴자리 계산
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		BigInteger bi1 = new BigInteger(sc.next());
+//		BigInteger bi2 = new BigInteger(sc.next());
+//		System.out.printf("%s\n%s\n%s", bi1.add(bi2), bi1.subtract(bi2), bi1.multiply(bi2));
+//	}
+	
+	// 큰 수 A+B
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		BigInteger bi1 = new BigInteger(sc.next());
+//		BigInteger bi2 = new BigInteger(sc.next());
+//		System.out.println(bi1.add(bi2));
+//	}
+	
+	// A + B + C
+//	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		System.out.println(sc.nextLong() + sc.nextLong() + sc.nextLong());
+//	}
+	
+	// 와이버스 부릉부릉
+//	public static void main(String[] args) {
+//		System.out.println("비와이");
+//	}
 	
 	// Julka
 //	public static void main(String[] args) {
