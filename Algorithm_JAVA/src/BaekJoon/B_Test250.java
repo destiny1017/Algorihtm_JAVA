a1691be8b1dfea44c01831dd11aef7f0dba5b14c
KDH_Notebook <destiny1017@nate.com>
2022-06-27 15:18:41 +0900

    2559 수열

---
+++ Algorithm_JAVA/src/BaekJoon/B_Test250.java
@@ -0,0 +1,40 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B_Test250 {
+	
+	// # 2559 수열
+	// 투포인터 문제
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int arr[] = new int[n];
+		int maxVal = 0;
+		int nowVal = 0;
+		
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		for (int i = 0; i < k; i++) {
+			nowVal += arr[i];
+		}
+		
+		maxVal = nowVal;
+		for (int i = k; i < n; i++) {
+			nowVal = nowVal + arr[i] - arr[i-k];
+			maxVal = Math.max(maxVal, nowVal);
+		}
+		
+		System.out.println(maxVal);
+	}
+
+}
