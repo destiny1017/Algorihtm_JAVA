059bbc52949b03b82a6935852fdb0e59d3c1cb02
KDH_Notebook <destiny1017@nate.com>
2022-10-22 17:27:14 +0900

    10816 숫자카드2, 시간초과 해결

--- Algorithm_JAVA/src/BaekJoon/B_Test255.java
+++ Algorithm_JAVA/src/BaekJoon/B_Test255.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class B_Test255 {
@@ -18,6 +19,8 @@ public class B_Test255 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
+		HashMap<Integer, Integer> countMap = new HashMap<>();
+		HashMap<Integer, Integer> indexMap = new HashMap<>();
 		int n = Integer.parseInt(st.nextToken());
 		ownCard = new int[n];
 		st = new StringTokenizer(br.readLine());
@@ -30,14 +33,23 @@ public class B_Test255 {
 		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < m; i++) {
 			int target = Integer.parseInt(st.nextToken());
-			int index = binarySearch(0, n-1, target);
-			int cnt = 0;
-			if(index >= 0) {
-				while(ownCard[index] == target) {
-					cnt++;
-					if(++index == ownCard.length) break;
-				}
+			if(indexMap.get(target) == null) {
+//				indexMap.put(target, binarySearch(0, n-1, target));
+				indexMap.put(target, lowerBound(target));
 			}
+			int index = indexMap.get(target);
+			
+			int cnt = 0;
+			if(countMap.get(target) == null) {
+				if(index >= 0) {
+					while(ownCard[index] == target) {
+						cnt++;
+						if(++index == ownCard.length) break;
+					}
+				}
+				countMap.put(target, cnt);
+			} else cnt = countMap.get(target);
+			
 			sb.append(cnt + " ");
 		}
 		System.out.println(sb.toString());
@@ -60,6 +72,21 @@ public class B_Test255 {
 		} else {
 			return binarySearch(mid+1, ed, target);
 		}
+	}
+	
+	static int lowerBound(int target) {
+		int begin = 0;
+		int end = ownCard.length-1;
+		
+		while(begin < end) {
+			int mid = (begin + end) / 2;
+			if(ownCard[mid] >= target) {
+				end = mid;
+			} else {
+				begin = mid + 1;
+			}
+		}
+		return end;
 	}
 
 }
